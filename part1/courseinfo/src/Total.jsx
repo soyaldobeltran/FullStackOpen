8eaa118a8c592d8bb47a3902504f710e16e837b9
soyaldobeltran <andresaldo45@gmail.com>
2025-03-27 17:50:43 -0500

    Eliminar archivos y configuraciones de React y Vite

--- part1/courseinfo/src/Total.jsx
+++
@@ -1,7 +0,0 @@
-const Total = (props) => {
-    return(
-        <p>Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
-    )
-}
-
-export default Total
